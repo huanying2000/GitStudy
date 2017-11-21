aaa3c88bae18d2d34a0e350168b616c18bede3bf
ios开发 <[715416878@qq.com]>
2017-11-21 17:37:53 +0800

    谢谢

--- GitStudy/GitStudy/ViewController.swift
+++ GitStudy/GitStudy/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("我们一起看月亮爬上来")
         print("胆小鬼1")
+        print("谢谢")
     }
 
     override func didReceiveMemoryWarning() {
